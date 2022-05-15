756aafa863b67524cba45d1e9c09f35a7db45ff2
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-05-15 10:24:17 +0300

    Admin Panel Setup Part 2

--- resources/views/admin/body/footer.blade.php
+++ resources/views/admin/body/footer.blade.php
@@ -2,13 +2,27 @@
                     <div class="container-fluid">
                         <div class="row">
                             <div class="col-sm-6">
-                                 EasyLearning © Upcube.
+                                <p id="now" ></p> 
                             </div>
                             <div class="col-sm-6">
                                 <div class="text-sm-end d-none d-sm-block">
-                                    Crafted with <i class="mdi mdi-heart text-danger"></i> by EasyLearning
+                                    Crafted with <i class="mdi mdi-heart text-danger"></i> by Riyad Mohammed Salem
                                 </div>
                             </div>
                         </div>
                     </div>
                 </footer>
+
+
+<script>
+   const now = new Date();
+   const FullDate =  now.toLocaleTimeString("en-us", {
+    day: "numeric",
+    month: "short",
+    hour: "2-digit",
+    minute: "2-digit",
+  }); 
+  const Year = now.getFullYear();
+
+document.querySelector('#now').innerText = `${FullDate} | ${Year} © Upcube.`;
+</script>
