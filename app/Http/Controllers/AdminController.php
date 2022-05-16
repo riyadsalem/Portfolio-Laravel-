7c3e0ef7bcc64861c1749c21f7b3a97d06838544
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-05-16 18:07:55 +0300

    Display Toster Message in login and Logout

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -18,7 +18,12 @@ class AdminController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect('/login');
+        $notification = array(
+            'message' => 'User Logout Successfully',
+            'alert-type' => 'success',
+        );
+
+        return redirect('/login')->with($notification);
     } // End Method
 
     public function Profile(){
