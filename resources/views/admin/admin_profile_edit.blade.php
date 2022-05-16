b5c4046c96299f5380d0b866fd8851b4be805797
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-05-16 15:38:40 +0300

    Admin Profile & Image Update Part 3

--- resources/views/admin/admin_profile_edit.blade.php
+++ resources/views/admin/admin_profile_edit.blade.php
@@ -1,6 +1,6 @@
 @extends('admin.admin_master')
 @section('admin')
-
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
 <div class="page-content">
 <div class="container-fluid">
@@ -44,7 +44,7 @@
                     <div class="row mb-3">
                         <label for="example-text-input" class="col-sm-2 col-form-label">Profile Image</label>
                         <div class="col-sm-10">
-                            <input name="profile_image" class="form-control" type="file" id="example-text-input">
+                            <input name="profile_image" class="form-control" type="file" id="image" >
                         </div>
                     </div>
                     <!-- end row -->
@@ -53,7 +53,7 @@
                     <label for="example-text-input" class="col-sm-2 col-form-label"></label>
                     <div class="col-sm-10">
 
-                    <img class="rounded avatar-lg" src="{{ asset('backend/assets/images/small/img-5.jpg') }}" alt="Card image cap">
+                    <img class="rounded avatar-lg" id="showImage" src="{{ asset('backend/assets/images/small/img-5.jpg') }}" alt="Card image cap">
 
                     </div>
                     </div>
@@ -72,5 +72,17 @@
 
 </div>
 </div>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('#image').change(function(e){
+            var reader = new FileReader();
+            reader.onload = function(e){
+                $('#showImage').attr('src',e.target.result);
+            }
+            reader.readAsDataURL(e.target.files['0']);
+        });
+    });
+</script>
 
 @endsection
