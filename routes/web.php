234a6ef0473f963e0c26bc0ab6efa8ee5ffa9039
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-05-16 14:51:18 +0300

    Admin Profile & Image Update Part 1

--- routes/web.php
+++ routes/web.php
@@ -28,5 +28,5 @@ require __DIR__.'/auth.php';
 // Admin All Route
 Route::controller(AdminController::class)->group(function(){  // Larael 9
     Route::get('/admin/logout','destroy')->name('admin.logout');
-
+    Route::get('/admin/profile','Profile')->name('admin.profile');
 });
