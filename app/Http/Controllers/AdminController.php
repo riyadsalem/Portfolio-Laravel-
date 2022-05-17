85d6f285741d86e0fec2d78d3a9dccc6933b9a5c
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-05-17 11:11:31 +0300

    Admin Profile Change Password Part 2

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 
 class AdminController extends Controller
@@ -79,10 +80,30 @@ class AdminController extends Controller
 
     public function UpdatePassword(Request $request){
 
+        $validateData = $request->validate([
+            'oldpassword' => 'required',
+            'newpassword' => 'required',
+            'confirm_password' => 'required|same:newpassword',
+        ]);
+
+        $hashedPassword = Auth::user()->password;
+        if(Hash::check($request->oldpassword,$hashedPassword)){
+            $users = User::find(Auth::id());
+            $users->password = bcrypt($request->newpassword);
+            $users->save();
+
+            session()->flash('message','Password Updated Successfully');
+            return redirect()->back();
+            
+        }else{
+            session()->flash('message','Old Password is not match');
+            return redirect()->back();
+        } // End if-else
+ 
     } // End Method
 
 
-    
+
 }
 
     
--- resources/views/admin/admin_change_password.blade.php
+++ resources/views/admin/admin_change_password.blade.php
@@ -12,6 +12,15 @@
 
                     <h4 class="card-title">Change Password Page</h4><br>
 
+                    @if(count($errors))
+                    @foreach($errors->all() as $error)
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert"> 
+                        {{ $error }}
+                      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                     </div>
+                    @endforeach
+                    @endif
+
                 <form action="{{ route('update.password') }}" method="post">
                     @csrf
 
