a995a3eb42d2c20b3ffc682403444a7bb51d92b3
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-05-20 15:06:23 +0300

    Backend Blog Page Setup Part 5

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -11,5 +11,9 @@ class Blog extends Model
 
     protected $guarded = [];
 
+    public function category(){
+        return $this->belongsTo(BlogCategory::class,'blog_category_id','id');
+    } // End Method
+
     
 }
