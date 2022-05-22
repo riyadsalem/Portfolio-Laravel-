065f0be772c250a6170549c872a1b56a612dc706
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-05-22 10:05:25 +0300

    Form Validation With JaveScript Part 2

--- resources/views/admin/blog_category/blog_category_add.blade.php
+++ resources/views/admin/blog_category/blog_category_add.blade.php
@@ -14,13 +14,13 @@
 
                     <h4 class="card-title">Add Blog Category Page</h4><br>
 
-                <form action="{{ route('store.blog.category') }}" method="post">
+                <form action="{{ route('store.blog.category') }}" method="post" id="myForm" >
                     @csrf
 
                     <div class="row mb-3">
                         <label for="example-text-input" class="col-sm-2 col-form-label">
                             Blog Category Name</label>
-                        <div class="col-sm-10">
+                        <div class="form-group col-sm-10">
                             <input name="blog_category" class="form-control" type="text" id="example-text-input">
 
                             <!--  
@@ -32,7 +32,6 @@
                         </div>
                     </div>
                     <!-- end row -->
-
 
                     <input type="submit" class="btn btn-info waves-effect waves-light" 
                     value="Insert Blog Category">
@@ -49,6 +48,32 @@
 </div>
 
 <script type="text/javascript">
+
+    $(document).ready(function(){
+        $('#myForm').validate({
+            rules: {
+                blog_category : {
+                    required: true,
+                },
+            },
+            messages: {
+                blog_category: {
+                    requird : 'Please Enter Blog Category',
+                },
+            },
+            errorElement: 'span',
+            errorPlacement: function(error,element){
+                error.addClass('invalid-feedback');
+                element.closest('.form-group').append(error);
+            },
+            highlight: function(element, errorClass, validClass){
+                $(element).addClass('is-invalid');
+            },
+            unhighlight: function(element,errorClass, validClass){
+                $(element).removeClass('is-invalid');
+            },
+        });
+    });
 
 </script>
 
